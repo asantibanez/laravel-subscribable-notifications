fd8f62a8eb3dafd3ed8e17528c4de7a043983844
Andrés Santibáñez <santibanez.andres@gmail.com>
2021-10-04 22:24:46 -0500

    Fixed imports

--- tests/TestModels/User.php
+++ tests/TestModels/User.php
@@ -3,15 +3,14 @@
 
 namespace Asantibanez\LaravelSubscribableNotifications\Tests\TestModels;
 
-use Asantibanez\LaravelSubscribableNotifications\Contracts\NotificationSubscriber;
 use Asantibanez\LaravelSubscribableNotifications\Tests\database\factories\UserFactory;
 use Asantibanez\LaravelSubscribableNotifications\Traits\HasNotificationSubscriptions;
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Model implements AuthorizableContract, AuthenticatableContract
