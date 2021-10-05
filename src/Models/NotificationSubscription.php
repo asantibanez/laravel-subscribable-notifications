7a8fc548f4ff3522f0a40e2ec0cbdb2e235ec106
Andrés Santibáñez <santibanez.andres@gmail.com>
2021-10-04 22:24:39 -0500

    Improved doc blocks

--- src/Models/NotificationSubscription.php
+++ src/Models/NotificationSubscription.php
@@ -4,11 +4,15 @@ namespace Asantibanez\LaravelSubscribableNotifications\Models;
 
 use Asantibanez\LaravelSubscribableNotifications\Database\Factories\NotificationSubscriptionFactory;
 use Asantibanez\LaravelSubscribableNotifications\Facades\NotificationSubscriptionManager;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property mixed user
+ * @method static NotificationSubscriptionFactory factory(...$parameters)
+ * @method static Builder|NotificationSubscription query()
+ * @method static Builder|NotificationSubscription forType($type)
  */
 class NotificationSubscription extends Model
 {
--- src/Traits/HasNotificationSubscriptions.php
+++ src/Traits/HasNotificationSubscriptions.php
@@ -3,8 +3,14 @@
 namespace Asantibanez\LaravelSubscribableNotifications\Traits;
 
 use Asantibanez\LaravelSubscribableNotifications\Models\NotificationSubscription;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Trait HasNotificationSubscriptions
+ * @package Asantibanez\LaravelSubscribableNotifications\Traits
+ * @property-read Collection|NotificationSubscription[] $notificationSubscriptions
+ */
 trait HasNotificationSubscriptions
 {
     public function notificationSubscriptions(): HasMany
